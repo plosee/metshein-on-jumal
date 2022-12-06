837c83932a3aacf3cb49e82097a570e4bd423eaf
Marten Laane <113144588+plosee@users.noreply.github.com>
2022-12-06 04:36:22 -0800

    Add files via upload

---
+++ powershellad.ps1
@@ -0,0 +1,21 @@
+﻿#sama kataloog, mis skriptil
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+$users = import-csv $dir\AD_Kasutajad.csv | format-table
+
+$parentou = 'DC=LAANE,DC=LOCAL'
+$adfilter = Get-ADOrganizationalUnit -filter "distinguishedname -like '*'"
+foreach ($user in $users)
+
+{
+
+    $enimi = $user.Eesnimi
+    $pnimi = $user.Perekonnanimi
+    $okond += $user.Osakond
+    
+
+    if ($adfilter -ne $okond)   {
+    New-ADOrganizationalUnit -name $okond -path $parentou
+    }
+}
